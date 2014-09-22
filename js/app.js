06d8ae8eb05183a16bd4de6cdb46d6a59fa99eb9
toddhollen <toddhollen@gmail.com>
2014-09-22 13:10:49 -0600

    Updated links

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
 
-	
+	$('.resume-click').click('toggle', function(event){
+			event.preventDefault();
+			$('.resume-add').slideToggle(400);
+	});
+
+	$('.project-click').click('toggle', function(event){
+			event.preventDefault();
+			$('.project-list').slideToggle(400);
+	});
+
 });
