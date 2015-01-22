1c68ff82957f7f6e0814ef0d9ca41e61949da74d
toddhollen <toddhollen@gmail.com>
2015-01-22 15:26:36 -0700

    New Site Files Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,59 @@
 $(document).ready(function() {
+    $('.about').fadeIn();
 
-	$('.resume-click').click('toggle', function(event){
-			event.preventDefault();
-			$('.resume-add').slideToggle(400);
-	});
+// On Click "About" 
+// If .about is showing, do nothing
+// Else hide .resume or .project; Show .about with slide animations
 
-	$('.project-click').click('toggle', function(event){
-			event.preventDefault();
-			$('.project-list').slideToggle(400);
-	});
+$('.a-click').on('click', function(event){
+    
+    if( !$("section.about").is(":visible") ){
+
+    	$("section.resume, section.projects").fadeOut('400', function() {
+    		
+    	});
+
+        $("section.about").fadeIn('400', function() { $("section.about").show();  
+        });
+    }
+
+});
+
+// On Click "Resume" 
+// If .resume is showing, do nothing
+// Else hide .about or .project; Show .resume with slide animations
+
+$('.r-click').on('click', function(event){
+    
+    if( !$("section.resume").is(":visible") ){
+
+    	$("section.about, section.projects").fadeOut('400', function() {
+    		
+    	});
+
+        $("section.resume").fadeIn('400', function() {$("section.resume").show()
+        	
+        });
+    }
+
+});
+
+// On Click "Projects" 
+// If .projects is showing, do nothing
+// Else hide .resume or .about; Show .projects with slide animations
+
+$('.p-click').on('click', function(event){
+    
+    if( !$("section.projects").is(":visible") ){
+
+    	$("section.about, section.resume").fadeOut('400', function() {
+400    	});
+
+        $("section.projects").fadeIn('400', function() { $("section.projects").show()
+        	
+        });
+    }
+
+});
 
 });
